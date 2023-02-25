b0f2b50122b0da920752fb3d4f43070eed61a47c
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-02-25 16:10:22 -0300

    configurando stitches

---
+++ src/pages/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react';
+
+export const {
+    config, 
+    styled,
+    globalCss,
+    css,
+    keyframes,
+    getCssText,
+    theme,
+    createTheme,
+ } = createStitches({
+    theme: {
+        colors: {
+            rocket: '#8257e6'
+        }
+    }
+})
