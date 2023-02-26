0f46ee0323105ebb273ac47b54884fe6b1187bd3
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-02-26 01:29:37 -0300

    estrutura da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-import { styled } from "./styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "./styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px'
-})
+import camiseta1 from '../assets/1.png';
+import camiseta2 from '../assets/2.png';
+import camiseta3 from '../assets/3.png';
 
 export default function Home() {
   return (
-    <Button>Hello World</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="camisa1" />
+
+        <footer>
+          <span>Camiseta maratona explorer</span>
+          <strong>R$ 79,90</strong>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="camisa1" />
+
+        <footer>
+          <span>Camiseta Ignite</span>
+          <strong>R$ 79,90</strong>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
