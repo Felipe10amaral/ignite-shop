ecdb4e3666107f070b9f18055effce4fe82c25b9
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-02-26 00:31:37 -0300

    cabecalho da aplicacao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from './styles/global'
 
+import Logo from '../assets/Logo.svg';
+import { Container, Header } from './styles/pages/app';
+
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={Logo.src} alt="logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>  
+  )
   
 }
